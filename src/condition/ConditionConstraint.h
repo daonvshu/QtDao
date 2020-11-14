9b78f7be9027f36ce08c983003dddf9f6eaae23f
hydrone <1286772338@qq.com>
2020-11-14 18:39:16 +0800

    添加ConditionConstraint用于条件后缀函数

---
+++ src/condition/ConditionConstraint.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+class ConditionConstraint {
+public:
+    static ConditionConstraint limit(int a, int b);
+    static ConditionConstraint limit(int a);
+
+    static ConditionConstraint orderBy();
+
+    static ConditionConstraint groupBy();
+
+    static ConditionConstraint having();
+};
