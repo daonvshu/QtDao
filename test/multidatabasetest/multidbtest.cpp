6d07b0af7e25f306578993b7b883f3e4c12da432
daonvshu <1286772338@qq.com>
2025-03-04 09:55:19 +0800

    pass multiply database connect

--- test/multidatabasetest/multidbtest.cpp
+++ test/multidatabasetest/multidbtest.cpp
@@ -10,11 +10,13 @@
 #define SESSION_SQLITE2     2
 #define SESSION_MYSQL       3
 #define SESSION_SQLSERVER   4
+#define SESSION_PSQL        5
 
 #include "sqlite/v1/artist.h"
 #include "sqlite/v2/track.h"
 #include "mysql/artist.h"
 #include "sqlserver/artist.h"
+#include "psql/artist.h"
 
 #include "utils/testconfigloader.h"
 
@@ -60,6 +62,18 @@ void MultiDbTest::setupTest() {
                 .password(sqlServerConfigOption.password())
                 .configAlias("ms")
                 .session(SESSION_SQLSERVER)
+                .initializeDatabase();
+
+        const auto& psqlConfigOption = TestConfigLoader::instance().config().optionPSql();
+        dao::_config<dao::ConfigPSqlBuilder>()
+                .version(1)
+                .databaseName("multi_psqltest")
+                .host(psqlConfigOption.host())
+                .port(psqlConfigOption.port())
+                .user(psqlConfigOption.user())
+                .password(psqlConfigOption.password())
+                .configAlias("ps")
+                .session(SESSION_PSQL)
                 .initializeDatabase();
 
     } catch (dao::DaoException& e) {
@@ -120,6 +134,10 @@ void MultiDbTest::differentDbQueryTest() {
     Ms::Artist artistData6(3, "Pink Floyd3", 15);
     dao::_insert<Ms::Artist>(SESSION_SQLSERVER).build().insert({ artistData5, artistData6 });
 
+    Ps::Artist artistData7(2, "Led Zeppelin4", 15);
+    Ps::Artist artistData8(3, "Pink Floyd4", 15);
+    dao::_insert<Ps::Artist>(SESSION_PSQL).build().insert({ artistData7, artistData8 });
+
     SqlV1::Artist::Fields af;
 
     dao::_beginSession(SESSION_SQLITE1);
@@ -137,6 +155,11 @@ void MultiDbTest::differentDbQueryTest() {
     QCOMPARE(artistQuery3, artistData5);
     dao::_endSession();
 
+    dao::_beginSession(SESSION_PSQL);
+    auto artistQuery4 = dao::_select<Ps::Artist>().filter(af.id == 2).build().unique();
+    QCOMPARE(artistQuery4, artistData7);
+    dao::_endSession();
+
     {
         DAO_SCOPED_SESSION_BEGIN(SESSION_SQLITE1);
         artistQuery1 = dao::_select<SqlV1::Artist>().filter(af.id == 2).build().unique();
@@ -145,6 +168,11 @@ void MultiDbTest::differentDbQueryTest() {
             DAO_SCOPED_SESSION_BEGIN(SESSION_MYSQL);
             {
                 DAO_SCOPED_SESSION_BEGIN(SESSION_SQLSERVER);
+                {
+                    DAO_SCOPED_SESSION_BEGIN(SESSION_PSQL);
+                    artistQuery4 = dao::_select<Ps::Artist>().filter(af.id == 3).build().unique();
+                    QCOMPARE(artistQuery4, artistData8);
+                }
                 artistQuery3 = dao::_select<Ms::Artist>().filter(af.id == 3).build().unique();
                 QCOMPARE(artistQuery3, artistData6);
             }
@@ -196,6 +224,7 @@ void MultiDbTest::cleanup() {
     dao::_delete<SqlV2::Track>(SESSION_SQLITE2).build().deleteBy();
     dao::_delete<Mys::Artist>(SESSION_MYSQL).build().deleteBy();
     dao::_delete<Ms::Artist>(SESSION_SQLSERVER).build().deleteBy();
+    dao::_delete<Ps::Artist>(SESSION_PSQL).build().deleteBy();
 }
 
 void MultiDbTest::cleanupTestCase() {
@@ -203,4 +232,5 @@ void MultiDbTest::cleanupTestCase() {
     dao::ConfigManager::getConfig(SESSION_SQLITE2)->getClient()->dropDatabase();
     dao::ConfigManager::getConfig(SESSION_MYSQL)->getClient()->dropDatabase();
     dao::ConfigManager::getConfig(SESSION_SQLSERVER)->getClient()->dropDatabase();
+    dao::ConfigManager::getConfig(SESSION_PSQL)->getClient()->dropDatabase();
 }
